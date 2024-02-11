82d760a9cb46c201207a4f1097135f66a1e258ae
sebastian <sebastian.alxr@gmail.com>
2024-02-11 11:23:27 +0100

    resize image

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ path="/home/pi/ePaper"
 LOCKFILE="$path/run.sh.lock"
 subject="http://192.168.178.73:8123/dashboard-test/0"
 api="http://192.168.178.69:3000/api/screenshot?"
-url="${api}resX=1200&resY=825&outFormat=png&waitTime=9000&isFullPage=false&dismissModals=true&url=$subject"
+url="${api}resX=300&resY=206&outFormat=png&waitTime=14000&isFullPage=false&dismissModals=true&url=$subject"
 kuma="http://192.168.178.69:3001/api/push/1q4lhpcbhN?status=up&msg=OK&ping="
 
 if [ -e "$LOCKFILE" ]; then
@@ -14,6 +14,7 @@ fi
 touch "$LOCKFILE"
 
 curl -o $path/screenshot.png "$url"
+convert $path/screenshot.png -resize 400% $path/screenshot.png
 convert $path/screenshot.png $path/pika.bmp
 
 sudo $path/driver/IT8951/IT8951 0 0 $path/pika.bmp
